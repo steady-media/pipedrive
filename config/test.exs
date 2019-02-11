3efb2d60f3b4835cf7aa457baaf9cb14e9c712d2
Mihai Târnovan <mihai.tarnovan@gmail.com>
2019-02-11 15:04:03 +0200

    Add test config

--- config/test.exs
+++ config/test.exs
@@ -1,1 +1,11 @@
+use Mix.Config
 
+config :exvcr, [
+  vcr_cassette_library_dir: "fixture/vcr_cassettes",
+  filter_sensitive_data: [
+    [pattern: ~s(email":".+?"), placeholder: ~s(email":"<EMAIL_REMOVED>")]
+  ],
+  filter_url_params: true,
+  filter_request_headers: [],
+  response_headers_blacklist: []
+]
