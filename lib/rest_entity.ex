fe7da22fbbfc8cd07d537536fb263844d11e4959
Mihai Târnovan <mihai.tarnovan@gmail.com>
2019-02-14 21:42:02 +0200

    Declare both arities of RESTEntity callbacks
    This is useful to avoid warnings when using Mox for mocking. Without this the mocks would only declare the full arity, leading to compilation warnings on call sites that use partial arity.

--- lib/rest_entity.ex
+++ lib/rest_entity.ex
@@ -1,9 +1,20 @@
 defmodule Pipedrive.RESTEntity do
   @moduledoc false
   alias Pipedrive.API
+  
+  # We declare both arities to avoid warnings during mocking
+  @callback list() :: API.response()
   @callback list(Keyword.t()) :: API.response()
+
+  @callback create(map()) :: API.response()
   @callback create(map(), Keyword.t()) :: API.response()
+
+  @callback update(map()) :: API.response()
   @callback update(map(), Keyword.t()) :: API.response()
+
+  @callback delete(String.t()) :: API.response()
   @callback delete(String.t(), Keyword.t()) :: API.response()
+
+  @callback delete_bulk(nonempty_list(String.t())) :: API.response()
   @callback delete_bulk(nonempty_list(String.t()), Keyword.t()) :: API.response()
 end
