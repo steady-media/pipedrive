79dd506915b95eef3f567179585c29fb1dce3948
Mihai Târnovan <mihai.tarnovan@gmail.com>
2019-02-14 20:14:42 +0200

    Capture and assert log on rate limit test

--- test/rate_limit_test.exs
+++ test/rate_limit_test.exs
@@ -2,22 +2,25 @@ defmodule Pipedrive.Test.RateLimit do
   use ExUnit.Case
   require Logger
   alias Pipedrive.{Helpers.RateLimit, Organizations}
+  import ExUnit.CaptureLog
 
   setup do
     bypass = Bypass.open()
     {:ok, bypass: bypass, bypass_url: "http://localhost:#{bypass.port}/v1"}
   end
 
-  test "rate limiting times out", %{bypass: bypass, bypass_url: bypass_url} do
+  test "rate limiting time out is configurable", %{bypass: bypass, bypass_url: bypass_url} do
     Bypass.expect_once(bypass, fn conn ->
       assert "/v1/organizations" == conn.request_path
       conn = Plug.Conn.put_resp_header(conn, "Retry-After", "2")
       Plug.Conn.resp(conn, 429, "")
     end)
 
-    {:error, :timeout} =
-      RateLimit.sleep_and_retry(&Organizations.list/1, [base_url: bypass_url],
-        timeout: :timer.seconds(1)
-      )
+    capture_log(fn ->
+      assert {:error, :timeout} =
+               RateLimit.sleep_and_retry(&Organizations.list/1, [base_url: bypass_url],
+                 timeout: :timer.seconds(1)
+               )
+    end) =~ "Rate limit exceeded"
   end
 end
